ec39509237921e090bfee8e7b36bfdaf0babb7db
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-28 22:47:51 +0100

    install dotenv. Inital Auth0 setup. Wrap application in Auth0 provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { UserProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+require('dotenv').config();
+
+//
 
 ReactDOM.render(
-  <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={process.env.DOMAIN}
+    clientId={process.env.CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 );
