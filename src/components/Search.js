baf366cc5b6f0c35eb3b09156b136fb5c0dfc4cb
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-28 19:26:25 +0100

    search component initial scaffolding

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
+
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input type="text" placeholder="enter guthub username ... " />
+            <button type="submit">search</button>
+          </div>
+        </form>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
