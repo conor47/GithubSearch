a8e1b8118f735836847cd7520835fb59adc05d94
Conor ladrigan <conorladrigan95@gmail.com>
2021-10-28 03:24:36 +0100

    add react router switch. add 404 page and routing for 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <h1>404</h1>
+      <h3>Sorry, this page does not exist</h3>
+      <Link to="/" className="btn">
+        Take me back
+      </Link>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
